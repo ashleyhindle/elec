a86e43f22d526c5dde661bd282ea0f01563c8c4d
Ashley Hindle <hey@ashleyhindle.com>
2025-02-20 11:48:59 +0000

    feat: dependency injection of DB into App

--- src/index.php
+++ src/index.php
@@ -11,4 +11,7 @@ require_once file_exists(__DIR__ . '/vendor/autoload.php')
 $dotenv = Dotenv\Dotenv::createImmutable(realpath(__DIR__ . '/..'));
 $dotenv->load();
 
-(new App($argv[1] ?? ''))->prompt();
+$db = new SQLite3('whc.db', SQLITE3_OPEN_READONLY);
+$db->enableExceptions(true);
+
+(new App(db: $db, ip: $argv[1] ?? '86.2.94.106'))->prompt();
