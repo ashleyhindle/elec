004cc48a7694e1271d4cfafdabcf7b273621b4b8
Ashley Hindle <hey@ashleyhindle.com>
2025-02-20 13:34:08 +0000

    tests: add basic App tests

--- tests/Unit/AppTest.php
+++ tests/Unit/AppTest.php
@@ -2,14 +2,61 @@
 
 declare(strict_types=1);
 
-test('ip is internal', function () {
-    $app = new App\App('192.168.1.1');
-    expect($app->isIpInternal('192.168.1.1'))->toBeTrue();
-    expect($app->isIpInternal('10.0.0.1'))->toBeTrue();
-    expect($app->isIpInternal('172.16.0.1'))->toBeTrue();
-    expect($app->isIpInternal('172.31.255.255'))->toBeTrue();
-    expect($app->isIpInternal(''))->toBeTrue();
+use App\App;
+use Laravel\Prompts\Key;
 
-    expect($app->isIpInternal('172.32.0.1'))->toBeFalse();
-    expect($app->isIpInternal('8.8.8.8'))->toBeFalse();
+test('keys work correctly', function () {
+    $db = new SQLite3(realpath(__DIR__ . '/../../whc.db'));
+    $app = new App($db, '127.0.0.1');
+    $app->getSitesNear(5, 52, 6);
+    $app->listenForKeys();
+
+    expect($app->siteIndex)->toBe(0);
+
+    $app->emit('key', Key::RIGHT_ARROW);
+    expect($app->siteIndex)->toBe(1);
+
+    $app->emit('key', Key::RIGHT_ARROW);
+    expect($app->siteIndex)->toBe(2);
+
+    $app->emit('key', Key::DOWN_ARROW);
+    expect($app->siteIndex)->toBe(5);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(4);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(3);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(2);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(1);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(0);
+
+    $app->emit('key', Key::LEFT_ARROW);
+    expect($app->siteIndex)->toBe(5);
+});
+
+test('q quits the app', function () {
+    $db = new SQLite3(realpath(__DIR__ . '/../../whc.db'));
+    $app = new App($db, '127.0.0.1');
+    App::fake();
+
+    $app->terminal()->shouldReceive('exit')->once();
+    $app->listenForKeys();
+    $app->emit('key', 'q');
+});
+
+test('category filter works correctly', function () {
+    $db = new SQLite3(realpath(__DIR__ . '/../../whc.db'));
+    $app = new App($db, '127.0.0.1');
+    $app->categories = ['Cultural'];
+    $app->getSitesNear(5, 52, 6);
+    $app->listenForKeys();
+
+    expect(collect($app->closestSites)->pluck('category')->unique()->toArray())->toBe(['Cultural']);
 });
